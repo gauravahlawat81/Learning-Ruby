7984cb300c34392a8fd47307056710f9045f4435
gauravahlawat81 <gauravahlawat81@gmail.com>
2020-12-24 18:34:09 +0530

    Created placeholder concern

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title,:body,:main_image,:thumb_image
+    include Placeholder
 
     def self.angular
         where(subtitle: 'Angular')
@@ -12,5 +13,5 @@ class Portfolio < ApplicationRecord
     def set_defaults
         self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
         self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
+      end
 end
